d4cc9a28fcd6c4198eef4e4d1b223fa4a722b581
Jordan Jackson <jordanpatrickjackson@gmail.com>
2017-02-24 13:47:40 -0500

    Root Motion Character Controller Project Creation

---
+++ Assets/Game/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Animator))]
+public class Movement : MonoBehaviour 
+{
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // turning
+
+        // jumping
+
+        // move
+        Move();
+    }
+
+    private void Move()
+    {
+        animator.SetFloat("Forward", Input.GetAxisRaw("Vertical"));
+    }
+
+    private void Jump()
+    {
+
+    }
+}
---
+++ Assets/Mixamo/Fuse/ReadMe.txt
@@ -0,0 +1,9 @@
+Hello World!
+
+Thank you for buying Mixamo Fuse through the Unity Asset store and becoming an amazing Mixamo Customer!
+This is just a placeholder Project to work with the Guidelines of the Unity Asset Store project publishing.
+
+You should soon be receiving an email prompting you with a few next steps about what to do to download your application.
+Have a fantastic day, if you have any comments, questions, or concerns feel free to fill out a Support form on our Support page at https://community.mixamo.com
+
+Have a good one, thanks again!
